9fa2f243ec13b82ed264ff26167e33a75892b4bf
fxm-dell7740 <dqfxm@outlook.com>
2020-05-06 21:22:01 +0800

    第二次同步测试7740

---
+++ del0506test.py
@@ -0,0 +1,18 @@
+#del0506计算连续质数之和，测试dell同步git第二次测试
+#工作仓库为hello-word
+def prime(m):
+    result = ""  #返回的结果
+    i = 0 #用来记录次数
+    start = int(m)+1
+    while i<5:  
+        for j in range(2,start):
+            if start%j == 0:
+                break
+        else:
+            result =result+str(start)+','
+            i+=1
+        start += 1
+    return result
+n = eval(input("请输入一个整数:"))
+m = prime(n)
+print(m[:-1])
